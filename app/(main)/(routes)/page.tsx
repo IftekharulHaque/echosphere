22e3511718a1deaa259135668b3a8701a08721c3
IftekharulHaque <iftekhar3927@gmail.com>
2024-02-20 09:51:04 +0600

    added dark mode

--- app/(main)/(routes)/page.tsx
+++ app/(main)/(routes)/page.tsx
@@ -1,9 +1,11 @@
 import { UserButton } from "@clerk/nextjs"
+import { ModeToggle } from "@/components/ui/modetoggle"
 
 export default function Home() {
   return (
     <div>
       <UserButton afterSignOutUrl="/" />
+      <ModeToggle />
     </div>
   )
 }
