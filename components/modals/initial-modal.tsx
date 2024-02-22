9dba6b6659f8265464bed436aaf90443bab54654
IftekharulHaque <iftekhar3927@gmail.com>
2024-02-22 23:40:50 +0600

    added components and initial pages and forms

---
+++ components/modals/initial-modal.tsx
@@ -0,0 +1,96 @@
+"use client"
+
+import * as z from "zod"
+import { zodResolver } from "@hookform/resolvers/zod"
+import { useForm } from "react-hook-form"
+import {
+  Dialog,
+  DialogContent,
+  DialogFooter,
+  DialogHeader,
+  DialogDescription,
+  DialogTitle,
+} from "@/components/ui/dialog"
+
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form"
+import { Input } from "@/components/ui/input"
+import { Button } from "@/components/ui/button"
+
+const formSchema = z.object({
+  name: z.string().min(1, {
+    message: "Server name is required",
+  }),
+  imageUrl: z.string().min(1, {
+    message: "Server image is required",
+  }),
+})
+
+const InitialMosdal = () => {
+  const form = useForm({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      name: "",
+      imageUrl: "",
+    },
+  })
+
+  const isLoading = form.formState.isSubmitting
+  const onSubmit = async (values: z.infer<typeof formSchema>) => {
+    console.log(values)
+  }
+
+  return (
+    <Dialog open>
+      <DialogContent className="bg-white text-black p-0 overflow-hidden">
+        <DialogHeader className="pt-8 px-6">
+          <DialogTitle className="text-2xl text-center font-bold ">
+            Create your server
+          </DialogTitle>
+          <DialogDescription className="text-center text-zinc-500">
+            Create your server with a name and an image. You can always change
+            this later.
+          </DialogDescription>
+        </DialogHeader>
+
+        <Form {...form}>
+          <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
+            <div className="space-y=8 px-6">
+              <div className="flex items-center justify-center text-center">
+                TODO: Upload Image
+              </div>
+              <FormField
+                control={form.control}
+                name="name"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel className="uppercase text-xs text-zinc-500 font-bold dark:text-secondary/70">
+                      Server Name
+                    </FormLabel>
+
+                    <FormControl>
+                      <Input
+                        disabled={isLoading}
+                        className="bg-zinc-300/50  border-o focus-visible:ring-0 text-black focus-visible: ring-offset-0"
+                        placeholder="Enter server name"
+                        {...field}
+                      />
+                    </FormControl>
+                  </FormItem>
+                )}
+              />
+            </div>
+          </form>
+        </Form>
+      </DialogContent>
+    </Dialog>
+  )
+}
+
+export default InitialMosdal
