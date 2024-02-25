fde2f98146b22b413f1734d68039a37d0a3f8eb0
IftekharulHaque <iftekhar3927@gmail.com>
2024-02-25 07:05:08 +0600

    Update dependencies and add axios

--- components/file-upload.tsx
+++ components/file-upload.tsx
@@ -15,7 +15,17 @@ interface FileUploadProps {
 export const FileUpload = ({ onChange, value, endpoint }: FileUploadProps) => {
   const fileType = value?.split(".").pop()
   if (value && fileType !== "pdf") {
-    return  <div className="relative h-20 w-20">hello</div>
+    return (
+      <div className="relative h-20 w-20">
+        <Image fill src={value} alt="upload" className="rounded-full" />
+        <button
+          className="bg-red-500 text-white p-1 rounded-full absolute top-0 right-0 shadow-sm"
+          type="button"
+        >
+          <X className="h-4 w-4" onClick={() => onChange("")} />
+        </button>
+      </div>
+    )
   }
 
   return (
