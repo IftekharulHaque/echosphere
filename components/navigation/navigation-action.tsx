45556ef4629a422080a3226126b71eec0c90d4e0
iftekharulHaqueNoor <iftekhar3927@gmail.com>
2024-02-26 15:44:29 +0600

    sidebar added

--- components/navigation/navigation-action.tsx
+++ components/navigation/navigation-action.tsx
@@ -5,14 +5,16 @@ import { ActionTooltip } from "@/components/ui/action-tooltip"
 const NavigationAction = () => {
   return (
     <div>
-      <button className="group flex items-center">
-        <div className="flex mx-3 p-2 rounded-full group-hover:transition-all overflow-hidden items-center justify-center bg-background dark:bg-neutral-700 group-hover:bg-emerald-500 ">
-          <Plus
-            className="group-hover:text-white transition text-emerald-500 "
-            size={25}
-          />
-        </div>
-      </button>
+      <ActionTooltip label="Add a server" align="center" side="right">
+        <button className="group flex items-center">
+          <div className="flex mx-3 p-2 rounded-full group-hover:transition-all overflow-hidden items-center justify-center bg-background dark:bg-neutral-700 group-hover:bg-emerald-500 ">
+            <Plus
+              className="group-hover:text-white transition text-emerald-500 "
+              size={25}
+            />
+          </div>
+        </button>
+      </ActionTooltip>
     </div>
   )
 }
