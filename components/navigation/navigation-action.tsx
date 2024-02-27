93df1b6b18b65909115a528e34310e6ef23eacab
iftekharulHaqueNoor <iftekhar3927@gmail.com>
2024-02-27 15:07:45 +0600

    added sidebar, adding server side bar

--- components/navigation/navigation-action.tsx
+++ components/navigation/navigation-action.tsx
@@ -2,11 +2,17 @@
 
 import { Plus } from "lucide-react"
 import { ActionTooltip } from "@/components/ui/action-tooltip"
+import { useModal } from "@/hooks/use-modal-store"
 const NavigationAction = () => {
+  const { onOpen } = useModal()
+
   return (
     <div>
       <ActionTooltip label="Add a server" align="center" side="right">
-        <button className="group flex items-center">
+        <button
+          className="group flex items-center"
+          onClick={() => onOpen("createServer")}
+        >
           <div className="flex mx-3 p-2 rounded-full group-hover:transition-all overflow-hidden items-center justify-center bg-background dark:bg-neutral-700 group-hover:bg-emerald-500 ">
             <Plus
               className="group-hover:text-white transition text-emerald-500 "
