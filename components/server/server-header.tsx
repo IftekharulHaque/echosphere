542fadae52116b18183dd53455de75e56093317a
iftekharulHaqueNoor <iftekhar3927@gmail.com>
2024-02-27 15:16:07 +0600

    added new dropdown menu item

--- components/server/server-header.tsx
+++ components/server/server-header.tsx
@@ -6,12 +6,15 @@ import {
   DropdownMenu,
   DropdownMenuContent,
   DropdownMenuItem,
+  DropdownMenuSeparator,
   DropdownMenuTrigger,
 } from "../ui/dropdown-menu"
 import {
   ChevronDown,
+  LogOut,
   PlusCircle,
   Settings,
+  Trash,
   UserPlus,
   Users,
 } from "lucide-react"
@@ -55,6 +58,19 @@ export const ServerHeader = ({ server, role }: ServerHeaderProps) => {
           <DropdownMenuItem className="px-3 py-2 text-sm cursor-pointer">
             Create Channel
             <PlusCircle className="h-4 w-4 ml-auto" />
+          </DropdownMenuItem>
+        )}
+        {isModerator && <DropdownMenuSeparator />}
+        {isAdmin && (
+          <DropdownMenuItem className="text-rose-500 px-3 py-2 text-sm cursor-pointer">
+            Delete Server
+            <Trash className="h-4 w-4 ml-auto" />
+          </DropdownMenuItem>
+        )}
+        {!isAdmin && (
+          <DropdownMenuItem className="text-rose-500 px-3 py-2 text-sm cursor-pointer">
+            Leave Server
+            <LogOut className="h-4 w-4 ml-auto" />
           </DropdownMenuItem>
         )}
       </DropdownMenuContent>
