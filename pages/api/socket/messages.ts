88e6a12145c66fd16d3ae82cb21f0cfb786d589a
iftekharulHaqueNoor <iftekhar3927@gmail.com>
2024-03-25 12:49:46 +0600

    message implementation underway

--- pages/api/socket/messages.ts
+++ pages/api/socket/messages.ts
@@ -1,7 +1,9 @@
+import { currentProfilePages } from "@/lib/current-profile-pages";
+import { db } from "@/lib/db";
 import { NextApiResponseServerIo } from "@/types";
 import { NextApiRequest } from "next";
 
-export default function handler(
+export default async function handler(
     req: NextApiRequest,
     res: NextApiResponseServerIo
 ) {
@@ -10,6 +12,57 @@ export default function handler(
     }
 
     try {
+        const profile = await currentProfilePages(req)
+        const { content, fileUrl } = req.body
+        const { serverId, channelId } = req.query
+
+        if (!profile) {
+            return res.status(401).json({ message: "Unauthorized" })
+        }
+        if (!serverId) {
+            return res.status(400).json({ message: "Server ID is required" })
+        }
+        if (!channelId) {
+            return res.status(400).json({ message: "Channel ID is required" })
+        }
+        if (!content) {
+            return res.status(400).json({ message: "Content is required" })
+        }
+
+        const server = await db.server.findFirst({
+            where: {
+                id: serverId as string,
+                members: {
+                    some: {
+                        profileId: profile.id
+                    }
+                }
+
+            },
+            include: {
+                members: true
+            }
+        })
+
+        if (!server) {
+            return res.status(404).json({ message: "Server not found" })
+        }
+
+        const channel = await db.channel.findFirst({
+            where: {
+                id: channelId as string,
+                serverId: serverId as string
+            }
+        })
+        if (!channel) {
+            return res.status(404).json({ message: "Channel not found" })
+        }
+
+        const member = await server.members.find((member) => member.profileId === profile.id)
+
+        if (!member) {
+            return res.status(404).json({ message: "Member not found" })
+        }
 
     } catch (error) {
         console.log("Messages_POST", error);
