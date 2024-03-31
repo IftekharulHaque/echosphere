b264818e8733989771e9920c8b7ba16ea10a5c91
iftekharulHaqueNoor <iftekhar3927@gmail.com>
2024-03-31 17:48:23 +0600

    Adding the chat module

---
+++ hooks/use-chat-query.ts
@@ -0,0 +1,32 @@
+import qs from "query-string"
+import { useParams } from "next/navigation"
+import { useInfiniteQuery } from "@tanstack/react-query"
+
+import { useSocket } from "@/components/providers/socket-provider"
+
+interface ChatQueryProps {
+    queryKey: string
+    apiUrl: string
+    paramKey: "channelId" | "conversationId"
+    paramValue: string
+
+}
+
+export const useChatQuery = ({
+    queryKey, apiUrl, paramKey, paramValue
+}: ChatQueryProps) => {
+    const { isConnected } = useSocket()
+
+    const params = useParams()
+
+    const fetchMessages = async ({ pageParam = undefined }) => {
+        const url = qs.stringifyUrl({
+            url: apiUrl,
+            query: {
+                cursor: pageParam,
+                [paramKey]: paramValue
+            }
+        }, { skipNull: true })
+    }
+
+}
